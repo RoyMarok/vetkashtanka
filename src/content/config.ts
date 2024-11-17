9c1ff80602aea345d7d3befc72a5f8d19b1d4de2
Anton Evstigneev <splu-ya@yandex.ru>
2024-11-17 23:36:42 +0300

    Complex Table render

--- src/content/config.ts
+++ src/content/config.ts
@@ -1,15 +1,22 @@
 import { z, defineCollection } from "astro:content";
 
-const pricesCollection = defineCollection({
+export const pricesCollection = defineCollection({
     type: 'data',
     schema: z.array(
         z.object({
             title: z.string(),
-            price: z.string(),
+            price: z.string().optional(),
+            options: z.array(
+                z.object({
+                    title: z.string(),
+                    price: z.string(),}
+                )
+            ).optional()
         }),
     )
 })
 
 export const collections = {
     'prices': pricesCollection,
+    'surgery': pricesCollection,
 };
